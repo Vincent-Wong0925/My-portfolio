c99d2a8b5cbc8814430ac3c59d15d9eebb2c6498
Vincent Wong <vincentwco2002@gmail.com>
2024-10-18 14:51:13 +0800

    Implemented photo galleries

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,6 +1,6 @@
 const dasmoto = ['dasmoto1', 'dasmoto2', 'dasmoto3'];
 const teaCozy = ['TeaCozy1', 'TeaCozy2', 'TeaCozy3', 'TeaCozy4'];
-const watches = ['Watches1', 'Watches2', 'Watches3', 'Watches4', 'Watches5'];
+const watches = ['Watches1', 'Watches2', 'Watches3', 'Watches4', 'Watches5', 'Watches6'];
 
 function makeGallery(srcArr) {
     return {
@@ -14,17 +14,19 @@ function makeGallery(srcArr) {
     };
 }
 
-dsamotoGallery = makeGallery(dasmoto);
+dasmotoGallery = makeGallery(dasmoto);
 teaCozyGallery = makeGallery(teaCozy);
 watchesGallery = makeGallery(watches);
-/*
-console.log(dsamotoGallery);
-console.log(teaCozyGallery);
-console.log(watchesGallery);
 
-for (let i = 0; i < 10; i++) {
-    console.log(dsamotoGallery.nextImg());
-    console.log(teaCozyGallery.nextImg());
-    console.log(watchesGallery.nextImg());
+const teaCozyImg = document.getElementById('teaCozyImg');
+const watchesImg = document.getElementById('watchesImg');
+const dasmotoImg = document.getElementById('dasmotoImg');
+
+function changeImg(event, dasmotoGallery){
+    newSrc = dasmotoGallery.nextImg();
+    event.target.src = newSrc;
 }
-*/
+
+teaCozyImg.addEventListener('click', (event) => { changeImg(event, teaCozyGallery); });
+watchesImg.addEventListener('click', (event) => { changeImg(event, watchesGallery); });
+dasmotoImg.addEventListener('click', (event) => { changeImg(event, dasmotoGallery); });
